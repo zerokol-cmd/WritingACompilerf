8cecf2feef5d64eee16af80e5b50d1bdee18352e
Zerokol <1@gmai.ru>
2022-11-16 14:34:49 +0300

    added a one for hello world lol)

---
+++ cmake-build-debug/CMakeFiles/CppCompiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppCompiler.dir/main.cpp.obj"
+  "CppCompiler.exe"
+  "CppCompiler.exe.manifest"
+  "CppCompiler.pdb"
+  "libCppCompiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppCompiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
